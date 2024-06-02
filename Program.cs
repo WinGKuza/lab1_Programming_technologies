82c2b1e32fb68fbe5e0e9aa1b1934e2d2542cf2f
Данил Кузнецов <danilkuzn2004@gmail.com>
2024-06-02 18:23:25 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            const string fileName = "../../file.bin";
+            const int arraySize = 10000;
+
+            VirtualMemoryController vmm = new VirtualMemoryController(fileName, arraySize);
+
+            vmm.Write(100, 1);
+            vmm.Write(5000, 2);
+
+            int value1 = vmm.Read(100);
+            int value2 = vmm.Read(5000);
+
+            Console.WriteLine($"Значение по индексу 100: {value1}");
+            Console.WriteLine($"Значение по индексу 5000: {value2}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ VirtualMemoryController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    public class Page
+    {
+        public int PageNumber { get; set; }
+        public bool IsModified { get; set; }
+        public byte[] Data { get; set; }
+        public byte[] BitMap { get; set; }
+
+        public Page(int size)
+        {
+            Data = new byte[size];
+            BitMap = new byte[size / sizeof(int)];
+        }
+
+        public int Read(int index)
+        {
+            int offset = index * sizeof(int);
+            return BitConverter.ToInt32(Data, offset);
+        }
+
+        public void Write(int index, int value)
+        {
+            int offset = index * sizeof(int);
+            byte[] bytes = BitConverter.GetBytes(value);
+            Buffer.BlockCopy(bytes, 0, Data, offset, bytes.Length);
+            BitMap[index / 8] |= (byte)(1 << (index % 8));
+            IsModified = true;
+        }
+    }
+
+    public class VirtualMemoryController
+    {
+        private const int PageSize = 512;
+        private const int PageCount = 3;  // Количество страниц в оперативной памяти
+        private const string Signature = "VM";
+        private string fileName;
+        private FileStream swapFile;
+        private Page[] pages;
+        private int arraySize;
+
+        public VirtualMemoryController(string fileName, int arraySize)
+        {
+            this.fileName = fileName;
+            this.arraySize = arraySize;
+
+            InitializeSwapFile();
+            pages = new Page[PageCount];
+
+            for (int i = 0; i < PageCount; i++)
+            {
+                pages[i] = new Page(PageSize);
+            }
+        }
+
+        private void InitializeSwapFile()
+        {
+            if (!File.Exists(fileName))
+            {
+                using (swapFile = new FileStream(fileName, FileMode.Create, FileAccess.ReadWrite))
+                {
+                    byte[] sig = System.Text.Encoding.ASCII.GetBytes(Signature);
+                    swapFile.Write(sig, 0, sig.Length);
+                    byte[] buffer = new byte[PageSize];
+                    int totalPages = (arraySize * sizeof(int) + PageSize - 1) / PageSize;
+                    for (int i = 0; i < totalPages; i++)
+                    {
+                        swapFile.Write(buffer, 0, buffer.Length);
+                    }
+                }
+            }
+            else
+            {
+                swapFile = new FileStream(fileName, FileMode.Open, FileAccess.ReadWrite);
+            }
+        }
+
+        private int GetPageIndex(int index)
+        {
+            return index / PageSize;
+        }
+
+        public int Read(int index)
+        {
+            int pageIndex = GetPageIndex(index);
+            Page page = LoadPage(pageIndex);
+            return page.Read(index % (PageSize / sizeof(int)));
+        }
+
+        public void Write(int index, int value)
+        {
+            int pageIndex = GetPageIndex(index);
+            Page page = LoadPage(pageIndex);
+            page.Write(index % (PageSize / sizeof(int)), value);
+        }
+
+        private Page LoadPage(int pageIndex)
+        {
+            Page page = pages[pageIndex % PageCount];
+
+            if (page.PageNumber != pageIndex)
+            {
+                if (page.IsModified)
+                {
+                    SavePage(page);
+                }
+                LoadPageFromDisk(page, pageIndex);
+            }
+
+            return page;
+        }
+
+        private void SavePage(Page page)
+        {
+            swapFile.Seek(page.PageNumber * PageSize + Signature.Length, SeekOrigin.Begin);
+            byte[] data = page.Data;
+            swapFile.Write(data, 0, data.Length);
+            page.IsModified = false;
+        }
+
+        private void LoadPageFromDisk(Page page, int pageIndex)
+        {
+            swapFile.Seek(pageIndex * PageSize + Signature.Length, SeekOrigin.Begin);
+            byte[] data = new byte[PageSize];
+            swapFile.Read(data, 0, data.Length);
+            page.Data = data;
+            page.PageNumber = pageIndex;
+        }
+    }
+
+}
